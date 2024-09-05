c3f37bf0125cba8343374a2cfdf11288683ed71f
Kate <163327584+kateboyd@users.noreply.github.com>
2024-09-05 10:51:17 +0100

    headline and section styling WIP

--- src/components/TransformCopy/index.jsx
+++ src/components/TransformCopy/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './styles.css';
 
 const TransformCopy = () => {
   return (
-    <div>
+    <div className="section-copy">
         <h2>Transform your brand</h2>
         <p>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
-        <h3>Learn more</h3>
+        <h3 className="learn-more-transform"> LEARN MORE </h3>
     </div>
   );
 }
